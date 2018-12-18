72ea0e94f1d462114364f2bfa49f2be14b911cde
Unknown <ek1g15@soton.ac.uk>
2018-12-18 18:33:20 +0000

    Fix x axis in row chart

--- script.js
+++ script.js
@@ -14,35 +14,37 @@ d3.csv("data/prison_nationality.csv").then(function(data) {
 		var ndx = crossfilter(data);
 		var nationalityDimension = ndx.dimension(function(d) {return d["Nationality"];});
 		var nationalityCount = nationalityDimension.group().reduceSum(function(d) {return d["Total"];});
-
-
+		
 		var prisonChart = dc.rowChart("#prison_row_chart");
 
 		prisonChart
 		.width(768)
-		.height(480)
-		.x(d3.scaleLinear())
-		.elasticX(true)
+		.height(500)
 		.dimension(nationalityDimension)
 		.group(nationalityCount)
 		.colors(accent)
+		.elasticX(false)
 		.colorAccessor(function(d,i) {
 			if (data[i]["UK Nationals"] === "Yes") {
 				return "native";
 			}
 			return "foreign";
 		})
+		.x(d3.scaleLinear().domain([0,80000]).range([0,prisonChart.width()]))
+		//.clipPadding(10)
 		.render();
-
-
-		var regionDimension  =ndx.dimension(function(d) {return d["Region"];} );
+	 	prisonChart.xAxis().scale(prisonChart.x())
+		AddXAxis(prisonChart, "Number of People");
+		
+		//bar graph
+		var regionDimension = ndx.dimension(function(d) {return d["Region"];} );
  		var nationalityPercentage = regionDimension.group().reduceSum(function(d) {return d["Total"]/d["Total in UK"]*100;});
 
 		var prisonChartBar = dc.barChart("#prison_bar_chart");
 
 		prisonChartBar
-		.width(768)
-		.height(480)
+		.width(500)
+		.height(400)
 		.xUnits(dc.units.ordinal)
 		.x(d3.scaleBand())
 		.elasticX(true)
@@ -67,43 +69,3 @@ d3.csv("data/prison_nationality.csv").then(function(data) {
 
 
 	});
-
-
-// d3.csv("data/prison_nationality_percentage.csv").then(function(data) {
-// 	console.log(data);
-
-//  	// data.forEach(function (x) {
-//  	// 	return {
-//  	// 		nationality: x["Nationality"],
-//  	// 		total: x["Total"]
-//  	// 	};
-//  	// });
-// 		//dimensions
-// 		var ndx = crossfilter(data);
-// 		var nationalityDimension = ndx.dimension(function(d) {return d["Nationality"];});
-// 		var nationalityPercentage = nationalityDimension.group().reduceSum(function(d) {return d["Number in Prisons"]/d["Total in UK"]*100;});
-
-
-// 		var prisonChartBar = dc.barChart("#prison_bar_chart");
-
-// 		prisonChartBar
-// 		.width(768)
-// 		.height(480)
-// 		.xUnits(dc.units.ordinal)
-// 		.x(d3.scaleBand())
-// 		.elasticX(true)
-// 		.dimension(nationalityDimension)
-// 		.group(nationalityPercentage)
-// 		.xAxisLabel("Nationality")
-// 		.yAxisLabel("Percentage")
-// 		// .label(function(d) {
-// 		// 	return d.data.value * 100 + '%';
-// 		// })
-// 		.render();
-
-// 		dc.config.defaultColors(d3.schemeAccent);
-
-
-
-
-// 	});
